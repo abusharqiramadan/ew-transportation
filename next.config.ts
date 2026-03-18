95e18ea9d52c080bcedb5783ba4a809ed004e9c8
unknown <noelramadan06@gmail.com>
2026-03-17 23:13:14 -0600

    Changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/muslim-web' : '',
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },
